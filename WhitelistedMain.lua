aea4b8edab04726f3b51e687f08ef18601da140f
fuckscripts <95951241+fuckscripts@users.noreply.github.com>
2021-12-17 10:24:15 -1000

    Update WhitelistedMain.lua

--- WhitelistedMain.lua
+++ WhitelistedMain.lua
@@ -1,8 +1,9 @@
 --// Buy alt controll, dm Homolife#1525 discord.gg/encrypt
 local encryptdroppers = {3060735945}
+local dollyalts = {1548566513,2734521236,2413245637,1667832645,2230506077,519949514}
 
 local finaltable = {}
-for i,v in pairs(encryptdroppers) do
-	table.insert(finaltable,v)
-end
+for i,v in pairs(encryptdroppers) do table.insert(finaltable,v) end
+for i,v in pairs(dollyalts) do table.insert(finaltable,v) end
+
 return finaltable
