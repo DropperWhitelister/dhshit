ed4b645ebfb27245da9fd787f9f147d15bf8353d
fuckscripts <95951241+fuckscripts@users.noreply.github.com>
2021-12-17 16:42:42 -1000

    Update WhitelistedMain.lua

--- WhitelistedMain.lua
+++ WhitelistedMain.lua
@@ -2,10 +2,12 @@
 local encryptdroppers = {3060735945}
 local dollyalts = {1548566513,2734521236,2413245637,1667832645,2230506077,519949514}
 local dirtdroppers = {54338663,3070125955,3070127051,3071442138,3071442138,1457641001,3056681235}
+local yasou = {3092376008,3092363653,3092379948,3092382776,151296912,755221370}
 
 local finaltable = {}
 for i,v in pairs(encryptdroppers) do table.insert(finaltable,v) end
 for i,v in pairs(dollyalts) do table.insert(finaltable,v) end
 for i,v in pairs(dirtdroppers) do table.insert(finaltable,v) end
+for i,v in pairs(yasou) do table.insert(finaltable,v) end
 
 return finaltable
